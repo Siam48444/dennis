cb8b6d28251e78cadb45d4dc6c8254886f38dbc1
Siam <135020238+Siam48444@users.noreply.github.com>
2023-11-19 17:57:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function HamburgerButtonAppears() {
     };
 };
 window.addEventListener("scroll", HamburgerButtonAppears);
+
+
+
+// Elements Appear in Scroll
+function AppearInScroll() {
+    const detailsSection = document.querySelector(".detailsSection");
+    const detailsSectionPosition = detailsSection.getBoundingClientRect().top;
+    const windowPosition = window.innerHeight / 2.1;
+
+    if(detailsSectionPosition < windowPosition) {
+        detailsSection.classList.add("scrollAppeared");
+    } else {
+        detailsSection.classList.remove("scrollAppeared"); 
+    };
+};
+window.addEventListener("scroll", AppearInScroll);
