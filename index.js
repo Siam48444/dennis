db1c6a9339ce228770070e0e9d3b11742f31f28c
Siam <135020238+Siam48444@users.noreply.github.com>
2023-11-21 17:34:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Hamburger Button Appears
-function HamburgerButtonAppears() {
-    const hamburgerMenuButton = document.querySelector(".hamburgerMenuButton");
-    const heroSection = document.querySelector(".heroSection");
-    const heroSectionPosition = heroSection.getBoundingClientRect().bottom; 
-    const windowPosition = window.innerHeight / 1.2;
+// function HamburgerButtonAppears() {
+//     const hamburgerMenuButton = document.querySelector(".hamburgerMenuButton");
+//     const heroSection = document.querySelector(".heroSection");
+//     const heroSectionPosition = heroSection.getBoundingClientRect().bottom; 
+//     const windowPosition = window.innerHeight / 1.2;
     
-    if(heroSectionPosition < windowPosition) {
-        hamburgerMenuButton.classList.add("hamburgerMenuButtonAppeared");
-    } else {
-        hamburgerMenuButton.classList.remove("hamburgerMenuButtonAppeared");
-    };
-};
-window.addEventListener("scroll", HamburgerButtonAppears);
+//     if(heroSectionPosition < windowPosition) {
+//         hamburgerMenuButton.classList.add("hamburgerMenuButtonAppeared");
+//     } else {
+//         hamburgerMenuButton.classList.remove("hamburgerMenuButtonAppeared");
+//     };
+// };
+// window.addEventListener("scroll", HamburgerButtonAppears);
 
 
 
